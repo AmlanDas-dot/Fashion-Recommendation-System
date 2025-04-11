992bed56f28f1b4335990285ac0d42dbd746d624
Amlan Das <amlandasmain@gmail.com>
2025-04-11 18:09:45 +0530

    minor changes in order to access other files

--- app/gui_app.py
+++ app/gui_app.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import streamlit as st
 from PIL import Image
 from utils.outfit_generator import get_similar_items
